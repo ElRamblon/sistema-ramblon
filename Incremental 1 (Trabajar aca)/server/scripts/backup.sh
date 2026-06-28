f7565dab7e9e42ae44c06f8c3a010598f22cdb4d
Aura Systems <proyectouturestaurante@gmail.com>
2026-06-28 15:07:41 -0300

    borramos el nombre viejo

--- Incremental 1 (Trabajar aca)/server/scripts/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FECHA=$(date +%Y-%m-%d_%H-%M-%S)
-tar -czf /var/backups/ramblon/backup_$FECHA.tar.gz /var/www/ramblon
-echo "Backup hecho: $FECHA"#!/bin/bash
-FECHA=$(date +%Y-%m-%d_%H-%M-%S)
-tar -czf /var/backups/ramblon/backup_$FECHA.tar.gz /var/www/ramblon
-echo "Backup hecho: $FECHA"
